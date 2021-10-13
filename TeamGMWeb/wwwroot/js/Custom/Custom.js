a38ed9ab3825e2a62197e688865e609b8738daf4
Leonardo Rocha <leonardo@eosconsultores.com.br>
2021-10-12 23:21:51 -0400

    subindo tela login

---
+++ TeamGMWeb/wwwroot/js/Custom/Custom.js
@@ -0,0 +1,8 @@
+﻿function dbox(msg) {
+    // (B1) GET ELEMENTS
+    var box = document.getElementById("dbox"),
+        boxm = document.getElementById("dboxm");
+    // (B2) SHOW/HIDE
+    boxm.innerHTML = (msg === undefined) ? "" : msg;
+    box.style.display = (msg === undefined) ? "none" : "block";
+}
